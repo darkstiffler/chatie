2eb3242a4d2a094b684ad893f42f9783012b4aaa
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-21 20:58:33 -0700

    add rails 4, pg, -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatie::Application.config.secret_key_base = '61b38c1a805a7bb9fe1a1ce858d7d86f8eeca13a248fe632235691d043de1307737e64dffd1c6ef26ce253be8ab0f0e0acf821bca49b576d3c4c4e0673153217'
